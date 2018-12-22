b871ef114222a6622d9f96ba11fce3961ce308a0
Carlos Martínez <mrcaleche@gmail.com>
2018-12-21 21:47:02 -0600

    Added admin frontend project, removed unnecessary files from repository, added correct gitignores

--- backend/src/controllers/admin/AdminProductController.ts
+++ backend/src/controllers/admin/AdminProductController.ts
@@ -1,6 +1,7 @@
 import ApplicationContext from "../../app-context/ApplicationContext";
 import ProductService from '../../services/ProductService'
 import {Request, Response} from 'express'
+import {SuccessResponse, ErrorResponse} from '../responses'
 
 const getService = (): ProductService => {
   return ApplicationContext.getImpl('productService')
@@ -9,9 +10,26 @@ const getService = (): ProductService => {
 const listProducts = (req: Request, res: Response) => {
   getService().paginate(req.params.offset, 10)
     .then(data => {
-      res.json(data)
+      res.json(new SuccessResponse(data))
     })
-    .catch()
+    .catch(err => {
+      console.error('Error while getting products', err)
+      res.status(500)
+        .json(new ErrorResponse('Error while getting products'))
+    })
 }
 
-export {listProducts}
+const createProduct = (req: Request, res: Response) => {
+  getService().create(req.body)
+    .then(data => {
+      res.json(new SuccessResponse({
+        id: data._id
+      }))
+    })
+    .catch(err => {
+      console.error('Error while trying to get a product', err)
+      res.status(500).json(new ErrorResponse('Error while creating product'))
+    })
+}
+
+export {listProducts, createProduct}
